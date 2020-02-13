2d81e6a12e97d73a7a9c35a6b7b1e3756733ff6f
Akash Chhetri <akash.chhetri1@gmail.com>
2020-02-14 10:53:37 +1100

    02- Added functionality to extract word and display meaning.

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import json
 
 # load data.json
-content = json.load(open("data.json"))
+data = json.load(open("data.json"))
 
-print(type(content))
+# function to extract meaning of the word
+def word_meaning(word):
+    if word in data:
+        meaning = data[word]
+    else:
+        meaning = 'Word does not exits!'
+    return meaning
+
+# input user input
+user_input = input("Enter word for meaning and press enter: ")
+
+# print result
+result = word_meaning(user_input)
+if result:
+    print(result)
+else:
+    print("word does not exist")
