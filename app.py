48a7cdf1ff05ff5e720412e6d9fab78ca119b393
Akash Chhetri <akash.chhetri1@gmail.com>
2020-02-14 12:37:34 +1100

    04- used pyspellchecker library to add intelligent spell checker.

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 import json
+#pip3 install pyspellchecker
+from spellchecker import SpellChecker
 
 # load data.json
 data = json.load(open("data.json"))
+
+# spell checker object
+spell = SpellChecker()
 
 # function to extract meaning of the word
 def word_meaning(word):
     if word in data:
         meaning = data[word]
+    elif spell.correction(word) in data: 
+        print("\nOops we couldn't find '%s' but we found '%s' \n" % (word,spell.correction(word)))
+        check_word = input("Press Y/N for Yes or No:  ")
+        if(check_word.lower() == 'y'):
+            meaning = data[spell.correction(word)]
+        else:
+            meaning = '\n\nWord does not exits! Please double check it\n'
     else:
-        meaning = 'Word does not exits! Please double check it'
+        meaning = '\n\nWord does not exits! Please double check it\n'
     return meaning
 
 # input user input
-user_input = input("Enter word for meaning and press enter: ")
+user_input = input("\nEnter word for meaning and press enter:  ")
 
-# print result and convert all input into lower case
+# convert all input into lower case
 result = word_meaning(user_input.lower())
-if result:
-    print(result)
-else:
-    print("word does not exist")
+
+# output
+print("\nMeaning: \n")
+print(result)
